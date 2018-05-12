73d4386f79a09f0849dc487889a8167027868fcb
Your Name <you@example.com>
2018-05-12 14:51:52 +0800

    modified PPO alogrithm

--- scripts/trainingPPO.py
+++ scripts/trainingPPO.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 """
 A simple version of Proximal Policy Optimization (PPO) using single thread.
 Based on:
@@ -12,7 +13,9 @@ gym 0.9.2
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
-import gym
+from models.PPO import PPO
+from gazebo_px4_gym_ros import World as world
+
 
 EP_MAX = 1000
 EP_LEN = 200
@@ -29,96 +32,13 @@ METHOD = [
 ][1]        # choose the method for optimization
 
 
-class PPO(object):
 
-    def __init__(self):
-        self.sess = tf.Session()
-        self.tfstate = tf.placeholder(tf.float32, [None, S_DIM], 'state')
+env = world()
+env.seed(0)
 
-        # critic
-        with tf.variable_scope('critic'):
-            l1 = tf.layers.dense(self.tfstate, 100, tf.nn.relu)
-            self.v = tf.layers.dense(l1, 1)
-            self.tfdc_r = tf.placeholder(tf.float32, [None, 1], 'discounted_r')
-            self.advantage = self.tfdc_r - self.v
-            self.closs = tf.reduce_mean(tf.square(self.advantage))
-            self.ctrain_op = tf.train.AdamOptimizer(CRITIC_LEARNING_RATE).minimize(self.closs)
-
-        # actor
-        pi, pi_params = self._build_anet('pi', trainable=True)
-        oldpi, oldpi_params = self._build_anet('oldpi', trainable=False)
-        with tf.variable_scope('sample_action'):
-            self.sample_op = tf.squeeze(pi.sample(1), axis=0)       # choosing action
-        with tf.variable_scope('update_oldpi'):
-            self.update_oldpi_op = [oldp.assign(p) for p, oldp in zip(pi_params, oldpi_params)]
-
-        self.tfaction = tf.placeholder(tf.float32, [None, A_DIM], 'action')
-        self.tfadvantage = tf.placeholder(tf.float32, [None, 1], 'advantage')
-        with tf.variable_scope('loss'):
-            with tf.variable_scope('surrogate'):
-                # ratio = tf.exp(pi.log_prob(self.tfaction) - oldpi.log_prob(self.tfaction))
-                ratio = pi.prob(self.tfaction) / oldpi.prob(self.tfaction)
-                surr = ratio * self.tfadvantage
-            if METHOD['name'] == 'kl_pen':
-                self.tflam = tf.placeholder(tf.float32, None, 'lambda')
-                kl = tf.distributions.kl_divergence(oldpi, pi)
-                self.kl_mean = tf.reduce_mean(kl)
-                self.aloss = -(tf.reduce_mean(surr - self.tflam * kl))
-            else:   # clipping method, find this is better
-                self.aloss = -tf.reduce_mean(tf.minimum(
-                    surr,
-                    tf.clip_by_value(ratio, 1.-METHOD['epsilon'], 1.+METHOD['epsilon'])*self.tfadvantage))
-
-        with tf.variable_scope('atrain'):
-            self.atrain_op = tf.train.AdamOptimizer(ACTOR_LEARNING_RATE).minimize(self.aloss)
-
-        tf.summary.FileWriter("log/", self.sess.graph)
-
-        self.sess.run(tf.global_variables_initializer())
-
-    def update(self, s, a, r):
-        self.sess.run(self.update_oldpi_op)
-        adv = self.sess.run(self.advantage, {self.tfstate: s, self.tfdc_r: r})
-        # adv = (adv - adv.mean())/(adv.std()+1e-6)     # sometimes helpful
-
-        # update actor
-        if METHOD['name'] == 'kl_pen':
-            for _ in range(A_UPDATE_STEPS):
-                _, kl = self.sess.run(
-                    [self.atrain_op, self.kl_mean],
-                    {self.tfstate: s, self.tfaction: a, self.tfadvantage: adv, self.tflam: METHOD['lam']})
-                if kl > 4*METHOD['kl_target']:  # this in in google's paper
-                    break
-            if kl < METHOD['kl_target'] / 1.5:  # adaptive lambda, this is in OpenAI's paper
-                METHOD['lam'] /= 2
-            elif kl > METHOD['kl_target'] * 1.5:
-                METHOD['lam'] *= 2
-            METHOD['lam'] = np.clip(METHOD['lam'], 1e-4, 10)    # sometimes explode, this clipping is my solution
-        else:   # clipping method, find this is better (OpenAI's paper)
-            [self.sess.run(self.atrain_op, {self.tfstate: s, self.tfaction: a, self.tfadvantage: adv}) for _ in range(A_UPDATE_STEPS)]
-
-        # update critic
-        [self.sess.run(self.ctrain_op, {self.tfstate: s, self.tfdc_r: r}) for _ in range(C_UPDATE_STEPS)]
-
-    def _build_anet(self, name, trainable):
-        with tf.variable_scope(name):
-            l1 = tf.layers.dense(self.tfstate, 100, tf.nn.relu, trainable=trainable)
-            mu = 2 * tf.layers.dense(l1, A_DIM, tf.nn.tanh, trainable=trainable)
-            sigma = tf.layers.dense(l1, A_DIM, tf.nn.softplus, trainable=trainable)
-            norm_dist = tf.distributions.Normal(loc=mu, scale=sigma)
-        params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope=name)
-        return norm_dist, params
-
-    def choose_action(self, s):
-        s = s[np.newaxis, :]
-        a = self.sess.run(self.sample_op, {self.tfstate: s})[0]
-        return np.clip(a, -2, 2)
-
-    def get_v(self, s):
-        if s.ndim < 2: s = s[np.newaxis, :]
-        return self.sess.run(self.v, {self.tfstate: s})[0, 0]
-
-env = gym.make('Pendulum-v0').unwrapped
+obs_dim = 13
+act_dim = 4
+a_bound = 1
 ppo = PPO()
 all_ep_r = []
 
@@ -127,9 +47,9 @@ for ep in range(EP_MAX):
     buffer_s, buffer_a, buffer_r = [], [], []
     ep_r = 0
     for t in range(EP_LEN):    # in one episode
-        env.render()
         a = ppo.choose_action(s)
-        s_, r, done, _ = env.step(a)
+        a_extend = np.concatenate((a, np.zeros(4, dtype=int)))*1100
+        s_, r, done, _ = env.step(a_extend)
         buffer_s.append(s)
         buffer_a.append(a)
         buffer_r.append((r+8)/8)    # normalize reward, find to be useful
@@ -155,6 +75,3 @@ for ep in range(EP_MAX):
         "|Ep_r: %i" % ep_r,
         ("|Lam: %.4f" % METHOD['lam']) if METHOD['name'] == 'kl_pen' else '',
     )
-
-plt.plot(np.arange(len(all_ep_r)), all_ep_r)
-plt.xlabel('Episode');plt.ylabel('Moving averaged episode reward');plt.show()
